b0417414a5e8ee7af8054ed9f96fd8a75e61bfc3
sn0k3 <ih999@abv.bg>
2022-07-08 22:08:47 +0300

    KeyInputListener added

---
+++ src/input/KeyInputListener.java
@@ -0,0 +1,24 @@
+package input;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyInputListener extends KeyAdapter implements KeyListener {
+	private boolean[] keyPressed = new boolean[120];
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keyPressed[e.getKeyCode()] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keyPressed[e.getKeyCode()] = false;
+	}
+
+	public boolean isKeyPressed(int keyCode) {
+		return keyPressed[keyCode];
+	}
+	
+}
